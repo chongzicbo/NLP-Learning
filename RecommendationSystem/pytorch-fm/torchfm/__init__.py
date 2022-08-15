9980c73373ddb6de344b8e040f8ed9ab04ec4cc0
chengbo <361848641@qq.com>
2022-08-15 09:14:04 +0800

    recommendation system

---
+++ RecommendationSystem/pytorch-fm/torchfm/__init__.py
@@ -0,0 +1,7 @@
+#! -*- coding:utf-8 -*-
+"""
+@author: chengbo
+@software: PyCharm
+@file: __init__.py.py
+@time: 2022/8/14 13:43
+"""
---
+++ RecommendationSystem/pytorch-fm/torchfm/model/fm.py
@@ -0,0 +1,31 @@
+#! -*- coding:utf-8 -*-
+"""
+@author: chengbo
+@software: PyCharm
+@file: fm.py
+@time: 2022/8/14 13:43
+"""
+
+import torch
+from torchfm.layer import FactorizationMachine, FeaturesEmbedding, FeaturesLinear
+
+
+class FactorizationMachineModel(torch.nn.Module):
+    """
+    A pytorch implementation of Factorization Machine.
+    Reference:
+        S Rendle, Factorization Machines, 2010.
+    """
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
+        self.linear = FeaturesLinear(field_dims)
+        self.fm = FactorizationMachine(reduce_sum=True)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = self.linear(x) + self.fm(self.embedding(x))
+        return torch.sigmoid(x.squeeze(1))
