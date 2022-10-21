7c4ac6c6d0677e688ce1a951a14425a67afc76ab
chengbo <361848641@qq.com>
2022-10-21 17:43:17 +0800

    tf2 examples

--- tf2_tutorial/custom_layer.py
+++ tf2_tutorial/custom_layer.py
@@ -6,6 +6,7 @@
 @time: 2022/6/26 15:55
 """
 import tensorflow as tf
+from tensorflow.python import keras
 
 
 class MyDenseLayer(tf.keras.layers.Layer):
@@ -23,9 +24,9 @@ class MyDenseLayer(tf.keras.layers.Layer):
         return tf.matmul(inputs, self.kernel)
 
 
-layer = MyDenseLayer(10)
-_ = layer(tf.zeros([10, 5]))
-print([var.name for var in layer.trainable_variables])
+# layer = MyDenseLayer(10)
+# _ = layer(tf.zeros([10, 5]))
+# print([var.name for var in layer.trainable_variables])
 
 
 class ResnetIdentityBlock(tf.keras.Model):
@@ -58,7 +59,230 @@ class ResnetIdentityBlock(tf.keras.Model):
         return tf.nn.relu(x)
 
 
-block = ResnetIdentityBlock(1, [1, 2, 3])
-_ = block(tf.zeros([1, 2, 3, 3]))
+# block = ResnetIdentityBlock(1, [1, 2, 3])
+# _ = block(tf.zeros([1, 2, 3, 3]))
+#
+# print(block.layers)
+class Linear(keras.models.Layer):
+    def __init__(self, units=32, input_dim=32):
+        super(Linear, self).__init__()
+        w_init = tf.random_normal_initializer()
+        self.w = tf.Variable(initial_value=w_init(shape=(input_dim, units), dtype="float32"), trainable=True
+                             )
+        b_init = tf.zeros_initializer()
+        self.b = tf.Variable(initial_value=b_init(shape=(units,), dtype="float32"), trainable=True
+                             )
 
-print(block.layers)
+    def call(self, inputs, *args, **kwargs):
+        return tf.matmul(inputs, self.w) + self.b
+
+
+class Linear1(keras.models.Layer):
+    def __init__(self, units=32, input_dim=32):
+        super(Linear1, self).__init__()
+        self.w = self.add_weight(shape=(input_dim, units), initializer="random_normal", trainable=True
+                                 )
+        self.b = self.add_weight(shape=(units,), initializer="zeros", trainable=True)
+
+    def call(self, inputs, *args, **kwargs):
+        return tf.matmul(inputs, self.w) + self.b
+
+
+class Linear(keras.models.Layer):
+    def __init__(self, units=32):
+        super(Linear, self).__init__()
+        self.units = units
+
+    def build(self, input_shape):
+        self.w = self.add_weight(shape=(input_shape[-1], self.units), initializer="random_normal", trainable=True)
+        self.b = self.add_weight(shape=(self.units,), initializer="random_normal", trainable=True)
+
+    def call(self, inputs, *args, **kwargs):
+        return tf.matmul(inputs, self.w) + self.b
+
+
+class ComputeSum(keras.models.Layer):
+    def __init__(self, input_dim):
+        super(ComputeSum, self).__init__()
+        # 不可训练权重
+        self.total = tf.Variable(initial_value=tf.zeros((input_dim,)), trainable=False)
+
+    def call(self, inputs, *args, **kwargs):
+        self.total.assign_add(tf.reduce_sum(inputs, axis=0))
+        return self.total
+
+
+class MLPBlock(keras.models.Layer):
+    def __init__(self):
+        super(MLPBlock, self).__init__()
+        self.linear1 = Linear(32)
+        self.linear2 = Linear(32)
+        self.linear3 = Linear(1)
+
+    def call(self, inputs):
+        x = self.linear1(inputs)
+        x = tf.nn.relu(x)
+        x = self.linear2(x)
+        x = tf.nn.relu(x)
+        return self.linear3(x)
+
+
+class ActivityRegularizationLayer(keras.models.Layer):
+    def __init__(self, rate=1e-2):
+        super(ActivityRegularizationLayer, self).__init__()
+        self.rate = rate
+
+    def call(self, inputs, *args, **kwargs):
+        self.add_loss(self.rate * tf.reduce_sum(inputs))
+        return inputs
+
+
+class OuterLayer(keras.models.Layer):
+    def __init__(self):
+        super(OuterLayer, self).__init__()
+        self.activity_reg = ActivityRegularizationLayer(1e-2)
+
+    def call(self, inputs, *args, **kwargs):
+        return self.activity_reg(inputs)
+
+
+class OuterLayerWithKernelRegularizer(keras.layers.Layer):
+    def __init__(self):
+        super(OuterLayerWithKernelRegularizer, self).__init__()
+        self.dense = keras.layers.Dense(
+            32, kernel_regularizer=tf.keras.regularizers.l2(1e-3)
+        )
+
+    def call(self, inputs):
+        return self.dense(inputs)
+
+
+class LogisticEndpoint(keras.models.Layer):
+    def __init__(self, name=None):
+        super(LogisticEndpoint, self).__init__(name=name)
+        self.loss_fn = keras.losses.BinaryCrossentropy(from_logits=True)
+        self.accuracy_fn = keras.metrics.BinaryAccuracy()
+
+    def call(self, targets, logits, sample_weights=None, *args, **kwargs):
+        loss = self.loss_fn(targets, logits, sample_weights)
+        self.add_loss(loss)
+
+        acc = self.accuracy_fn(targets, logits, sample_weights)
+        self.add_metric(acc, name="accuracy")
+
+        return tf.nn.softmax(logits)
+
+class Linear(keras.layers.Layer):
+    def __init__(self, units=32, **kwargs):
+        super(Linear, self).__init__(**kwargs)
+        self.units = units
+
+    def build(self, input_shape):
+        self.w = self.add_weight(
+            shape=(input_shape[-1], self.units),
+            initializer="random_normal",
+            trainable=True,
+        )
+        self.b = self.add_weight(
+            shape=(self.units,), initializer="random_normal", trainable=True
+        )
+
+    def call(self, inputs):
+        return tf.matmul(inputs, self.w) + self.b
+
+    def get_config(self):
+        config = super(Linear, self).get_config()
+        config.update({"units": self.units})
+        return config
+
+if __name__ == '__main__':
+    x = tf.ones((2, 2))
+    linear_layer = Linear1(4, 2)
+    y = linear_layer(x)
+    print(y)
+
+    assert linear_layer.weights == [linear_layer.w, linear_layer.b]
+
+    # x = tf.ones((2, 2))
+    # my_sum = ComputeSum(2)
+    # y = my_sum(x)
+    # print(y.numpy())
+    # y = my_sum(x)
+    # print(y.numpy())
+    #
+    # print(len(my_sum.weights))
+    # print(len(my_sum.non_trainable_weights))
+    # print(len(my_sum.trainable_weights))
+    #
+    # linear_layer = Linear(32)
+    # y = linear_layer(x)
+    #
+    # print(y)
+    #
+    # mlp = MLPBlock()
+    # y = mlp(tf.ones(shape=(3, 64)))
+    # print("weights:", len(mlp.weights))
+    # print("trainable weights:", len(mlp.trainable_weights))
+
+    # layer = OuterLayer()
+    # assert len(layer.losses) == 0
+    # _ = layer(tf.zeros(1, 1))
+    # assert len(layer.losses) == 1
+    #
+    # _ = layer(tf.zeros(1, 1))
+    # assert len(layer.losses) == 1
+    #
+    # layer = OuterLayerWithKernelRegularizer()
+    # _ = layer(tf.zeros((1, 1)))
+    #
+    # # This is `1e-3 * sum(layer.dense.kernel ** 2)`,
+    # # created by the `kernel_regularizer` above.
+    # print(layer.losses)
+    #
+    # optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+    # loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    #
+    # # for x_batch_train,y_batch_train in train_da
+    # import numpy as np
+    #
+    # inputs = keras.Input(shape=(3,))
+    # outputs = ActivityRegularizationLayer()(inputs)
+    # model = keras.Model(inputs, outputs)
+    #
+    # # If there is a loss passed in `compile`, the regularization
+    # # losses get added to it
+    # model.compile(optimizer="adam", loss="mse")
+    # model.fit(np.random.random((2, 3)), np.random.random((2, 3)))
+    #
+    # # It's also possible not to pass any loss in `compile`,
+    # # since the model already has a loss to minimize, via the `add_loss`
+    # # call during the forward pass!
+    # model.compile(optimizer="adam")
+    # model.fit(np.random.random((2, 3)), np.random.random((2, 3)))
+    # import numpy as np
+    #
+    # layer = LogisticEndpoint()
+    # targets = tf.ones((2, 2))
+    # logits = tf.ones((2, 2))
+    # y = layer(targets, logits)
+    # print("layer.metrics:", layer.metrics)
+    # print("current accuracy value:", float(layer.metrics[0].result()))
+    #
+    # inputs = keras.Input(shape=(3,), name="inputs")
+    # targets = keras.Input(shape=(10,), name="targets")
+    # logits = keras.layers.Dense(10)(inputs)
+    # predictions = LogisticEndpoint(name="predictions")(logits, targets)
+    #
+    # model = keras.Model(inputs=[inputs, targets], outputs=predictions)
+    # model.compile(optimizer="adam")
+    #
+    # data = {
+    #     "inputs": np.random.random((3, 3)),
+    #     "targets": np.random.random((3, 10)),
+    # }
+    # model.fit(data)
+
+    layer = Linear(64)
+    config = layer.get_config()
+    print(config)
+    new_layer = Linear.from_config(config)
---
+++ tf2_tutorial/tf_function.py
@@ -0,0 +1,61 @@
+#! -*- coding:utf-8 -*-
+"""
+@author: chengbo
+@software: PyCharm
+@file: tf_function.py
+@time: 2022/9/26 15:57
+"""
+
+import tensorflow as tf
+import timeit
+from datetime import datetime
+
+
+def a_regular_function(x, y, b):
+    x = tf.matmul(x, y)
+    x = x + b
+    return x
+
+
+a_function_that_uses_a_graph = tf.function(a_regular_function)
+
+x1 = tf.constant([[1.0, 2.0]])
+y1 = tf.constant([[2.], [3.0]])
+b1 = tf.constant(4.0)
+
+orig_value = a_regular_function(x1, y1, b1).numpy()
+tf_function_value = a_function_that_uses_a_graph(x1, y1, b1).numpy()
+
+assert (orig_value == tf_function_value)
+
+
+def inner_function(x, y, b):
+    x = tf.matmul(x, y)
+    x = x + b
+    return x
+
+
+# 使用tf.function将`outer_function`变成一个tensorflow `Function`。注意，之前的代码是将tf.function当作是函数来使用，这样是被当作了修饰符来使用。这两种方式都是被支持的。
+@tf.function
+def outer_function(x):
+    y = tf.constant([[2.0], [3.0]])
+    b = tf.constant(4.0)
+
+    return inner_function(x, y, b)
+
+
+# tf.function构建的graph中不仅仅包含了 `outer_function`还包含了它里面调用的`inner_function`。
+print(outer_function(tf.constant([[1.0, 2.0]])).numpy())
+@tf.function
+def my_relu(x):
+  return tf.maximum(0., x)
+
+# 下面对`my_relu` 的3次调用的数据类型都不同，所以生成了3个graph。这3个graph都被保存在my_relu这个tenforflow function中。
+print(my_relu(tf.constant(5.5)))
+print(my_relu([1, -1])) #python数组
+print(my_relu(tf.constant([3., -3.])))	# tf数组
+
+# 下面这两个调用就不会生成新的graph.
+print(my_relu(tf.constant(-2.5))) # 这个数据类型与上面的 `tf.constant(5.5)`一样.
+print(my_relu(tf.constant([-1., 1.]))) # 这个数据类型与上面的 `tf.constant([3., -3.])`一样。
+
