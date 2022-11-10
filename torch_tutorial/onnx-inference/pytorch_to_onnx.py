196d26567fdd7569fd332cfcdf7a0805f20eb848
chengbo <361848641@qq.com>
2022-11-10 16:52:49 +0800

    模型部署

---
+++ torch_tutorial/onnx-inference/pytorch_to_onnx.py
@@ -0,0 +1,40 @@
+#! -*- coding:utf-8 -*-
+"""
+@author: chengbo
+@software: PyCharm
+@file: pytorch_to_onnx.py
+@time: 2022/11/10 10:58
+"""
+import torch
+from torch import nn
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+model=torch.load("model.pt")
+torch.onnx.export(model,
+                  torch.randn(1, 28, 28).to(device),
+                  "fashion_mnist_model.onnx",
+                  input_names=["input"],
+                  output_names=["output"])
+
+import onnx
+
+onnx_model = onnx.load("fashion_mnist_model.onnx")
+onnx.checker.check_model(onnx_model)
+
+print(onnx_model)
---
+++ torch_tutorial/onnx-inference/text_classify/ag_news_inference.py
@@ -0,0 +1,44 @@
+#! -*- coding:utf-8 -*-
+"""
+@author: chengbo
+@software: PyCharm
+@file: ag_news_inference.py
+@time: 2022/11/10 11:31
+"""
+import torch
+from ag_news_classify import TextClassificationModel, train_iter, build_vocab_from_iterator, text_pipeline, yield_tokens, tokenizer, ag_news_label
+
+import torch
+
+text = "Text from the news article"
+text = torch.tensor(text_pipeline(text))
+offsets = torch.tensor([0])
+print(text)
+print(offsets)
+# Export the model
+model = torch.load("ag_new.pth")
+torch.onnx.export(model,  # model being run
+                  (text, offsets),  # model input (or a tuple for multiple inputs)
+                  "ag_news_model.onnx",  # where to save the model (can be a file or file-like object)
+                  export_params=True,  # store the trained parameter weights inside the model file
+                  opset_version=10,  # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names=['input', 'offsets'],  # the model's input names
+                  output_names=['output'],  # the model's output names
+                  dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
+                                'output': {0: 'batch_size'}})
+
+import onnx
+
+onnx_model = onnx.load("ag_news_model.onnx")
+onnx.checker.check_model(onnx_model)
+
+import onnxruntime as ort
+import numpy as np
+
+ort_sess = ort.InferenceSession('ag_news_model.onnx')
+outputs = ort_sess.run(None, {'input': text.numpy(),
+                              'offsets': torch.tensor([0]).numpy()})
+# Print Result
+result = outputs[0].argmax(axis=1) + 1
+print("This is a %s news" % ag_news_label[result[0]])
