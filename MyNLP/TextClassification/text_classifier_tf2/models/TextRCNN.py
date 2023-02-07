af4b001bd204d859b09708a37e80527abf0f0e96
chengbo <361848641@qq.com>
2023-02-07 16:37:35 +0800

    Delete MyNLP directory

--- MyNLP/TextClassification/text_classifier_tf2/models/TextRCNN.py
+++
@@ -1,7 +0,0 @@
-#! -*- coding:utf-8 -*-
-"""
-@author: chengbo
-@software: PyCharm
-@file: TextRCNN.py
-@time: 2022/10/31 10:33
-"""
--- MyNLP/TextClassification/text_classifier_tf2/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-#! -*- coding:utf-8 -*-
-"""
-@author: chengbo
-@software: PyCharm
-@file: __init__.py
-@time: 2022/10/31 10:54
-"""
