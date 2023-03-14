92daa3ce83f6aa778de8eea35ba0d6a7a4ce84ce
chengbo <bo.cheng@mdpi.com>
2023-03-14 16:14:23 +0800

    transformers学习

---
+++ python_learning/huggingface/transformers_test/trainer_test.py
@@ -0,0 +1,56 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : trainer_test.py
+# Time       ：2023/3/7 17:31
+# Author     ：chengbo
+# version    ：python 3.8
+# Description：
+"""
+import torch
+
+print(torch.cuda.is_available())
+
+from transformers import AutoModelForSequenceClassification
+
+model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+from transformers import TrainingArguments
+
+training_args = TrainingArguments(
+    output_dir='./model',
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=2,
+)
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+from datasets import load_dataset
+
+dataset = load_dataset('rotten_tomatoes')
+# print(dataset["train"]["text"])
+
+def tokenize_dataset(dataset):
+    return tokenizer(dataset['text'])
+
+
+dataset=dataset.map(tokenize_dataset,batched=True)
+
+from transformers import DataCollatorWithPadding
+data_collator=DataCollatorWithPadding(tokenizer=tokenizer)
+from transformers import Trainer
+
+trainer=Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset['train'],
+    eval_dataset=dataset['test'],
+    tokenizer=tokenizer,
+    data_collator=data_collator
+)
+
+trainer.train()
