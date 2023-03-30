c2bf05d2798edb07e9a4e972fada749526d08ae4
chengbo <bo.cheng@mdpi.com>
2023-03-30 14:20:14 +0800

    GPT2 pytorch

---
+++ PretrainedModel/GPT2-Pytorch/GPT2/config.py
@@ -0,0 +1,32 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : config.py
+# Time       ：2023/3/22 16:49
+# Author     ：chengbo
+# version    ：python 3.8
+# Description：
+"""
+
+
+class GPT2Config(object):
+    def __init__(self,
+                 vocab_size_or_config_json_file=50257,
+                 n_positions=1024,
+                 n_ctx=1024,
+                 n_embd=768,
+                 n_layer=12,
+                 n_head=12,
+                 layer_norm_epsilon=1e-5,
+                 initializer_range=0.02
+                 ):
+        self.vocab_size = vocab_size_or_config_json_file
+        self.n_ctx = n_ctx
+        self.n_positions = n_positions
+        self.n_embd = n_embd
+        self.n_layer = n_layer
+        self.n_head = n_head
+        self.layer_norm_epsilon = layer_norm_epsilon
+        self.inititalizer_range = initializer_range
+
+
