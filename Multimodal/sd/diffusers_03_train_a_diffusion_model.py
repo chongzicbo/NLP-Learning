de78fd2e98d90bc3f6171276c119671a71f78dac
bowen <bowen.liu@mdpi.com>
2023-12-21 08:23:24 +0000

    feat: stable diffusion

---
+++ Multimodal/sd/diffusers_03_train_a_diffusion_model.py
@@ -0,0 +1,36 @@
+import os
+
+os.environ["HUGGINGFACE_HUB_CACHE"] = "/data/sshadmin/bocheng/.cache/huggingface/hub/"
+from dataclasses import dataclass
+
+
+@dataclass
+class TrainingConfig:
+    image_size = 128  # the generated image resolution
+    train_batch_size = 16
+    eval_batch_size = 16  # how many images to sample during evaluation
+    num_epochs = 50
+    gradient_accumulation_steps = 1
+    learning_rate = 1e-4
+    lr_warmup_steps = 500
+    save_image_epochs = 10
+    save_model_epochs = 30
+    mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
+    output_dir = "ddpm-butterflies-128"  # the model name locally and on the HF Hub
+
+    # push_to_hub = True  # whether to upload the saved model to the HF Hub
+    # hub_model_id = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
+    # hub_private_repo = False
+    overwrite_output_dir = True  # overwrite the old model when re-running the notebook
+    seed = 0
+
+
+config = TrainingConfig()
+from datasets import load_dataset
+
+config.dataset_name = "huggan/smithsonian_butterflies_subset"
+dataset = load_dataset(
+    config.dataset_name,
+    split="train",
+    cache_dir="/data/sshadmin/bocheng/data/huggingface/data",
+)
