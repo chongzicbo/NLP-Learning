bba2ff6f53695d62dab4271414fee461ef0e79ef
chongzicbo <361848641@qq.com>
2024-05-08 16:38:58 +0800

    aaa

---
+++ OCR/.ipynb_checkpoints/trocr_test-checkpoint.py
@@ -0,0 +1,16 @@
+import os
+
+os.environ["XDG_CACHE_HOME"] = "/data/bocheng/.cache"
+os.environ["HUGGINGFACE_HUB_CACHE"] = "/data/bocheng/.cache/huggingface/hub/"
+
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-printed")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-printed")
+url = "/home/bocheng/dev/mylearn/nougat-latex-ocr/examples/test_data/0543053.png"
+image = Image.open(url).convert("RGB")
+pixel_values = processor(image, return_tensors="pt").pixel_values
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(generated_text)
