38119ae5b2b03af3e1cff1bd3eaf625818b05c07
chongzicbo <361848641@qq.com>
2024-08-02 17:24:11 +0800

    nicegui

---
+++ python_learning/nicegui_test/02-styling.py
@@ -0,0 +1,16 @@
+from nicegui import ui
+
+ui.icon("thumb_up")
+ui.markdown("This is **Markdown**.")
+ui.html("This is <strong>HTML</strong>.")
+with ui.row():
+    ui.label("CSS").style("color: #888; font-weight: bold")
+    ui.label("Tailwind").classes("font-serif")
+    ui.label("Quasar").classes("q-ml-xl")
+with ui.row():
+    ui.label("CSS").style("color: #888; font-weight: bold")
+    ui.label("Tailwind").classes("font-serif")
+    ui.label("Quasar").classes("q-ml-xl")
+ui.link("NiceGUI on GitHub", "https://github.com/zauberzeug/nicegui")
+
+ui.run()
