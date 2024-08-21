db055f5e7c6102e7e7ffce51297c70a27ffaa942
chongzicbo <361848641@qq.com>
2024-08-21 14:32:12 +0800

    feat:nicegui and fastapi

---
+++ python_learning/nicegui_test/fastapi_test/main.py
@@ -0,0 +1,15 @@
+import frontend
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    return {'Hello': 'World'}
+
+
+frontend.init(app)
+
+if __name__ == '__main__':
+    print('Please start the app with the "uvicorn" command as shown in the start.sh script')
